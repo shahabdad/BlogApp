947fcb07dea9df01d8a2064f94589bde02d95708
Shahabdad <shahabdad50@gmail.com>
2025-02-13 20:44:39 -0800

    PLease  error few time

--- models/BlogModel.js
+++ models/BlogModel.js
@@ -1,34 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const blogSchema = new mongoose.Schema(
-//     {
-//         title: {
-//             type: String,
-//             required: [true, "Title is required"],
-//         },
-//         description: {
-//             type: String,
-//             required: [true, "Description is required"],
-//         },
-//         image: {
-//             type: String,
-//             required: [true, "Image is required"],
-//         },
-  
-//         user: {
-//             type: mongoose.Types.ObjectId,
-//               ref: "User",
-//             require: [true, "user id is required"],
-//           },
-//     },
-
-//     { timestamps: true }
-// );
-
-// const blogModel = mongoose.model("blog", blogSchema);
-// module.exports = blogModel;
-
-
 const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema(
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,31 +1,3 @@
-// const mongoose = require('mongoose');
-// const userSchema  = new mongoose.Schema(
-//     {
-//         username  :{
-//             type:String,
-//             required:[true, "username is required"],
-//         },
-//         email:{
-//             type:String,
-//             require:[true, "email iis required"],
-//         },
-//         password:{
-//             type:String,
-//              required:[true,"password is required"],
-//         },      
-//         blogs: [
-//             {
-//               type: mongoose.Types.ObjectId,
-//               ref: "b   log",
-//             },
-//           ],
-//     },
-//         {timestamps:true}
-// );
-// const userModel = mongoose.model('User',userSchema);
-// module.exports = userModel;
-
-
 
 const mongoose = require("mongoose");
 
